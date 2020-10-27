5af948d9c29ec790752f660a44fdf98531e9612f
QWei <30896013+00x16@users.noreply.github.com>
2020-10-27 14:30:02 +0800

    Update weread.js

--- weread.js
+++ weread.js
@@ -1,6 +1,7 @@
 var body = $response.body;
 var url = $request.url;
-var obj = JSON.parse(body);
+//var obj = JSON.parse(body); //var its wrong let its right
+let obj = JSON.parse(body);
 if (url.indexOf('/pay/memberCardSummary') != -1) {
 	obj.remainTime = 9999999999999;
 	body = JSON.stringify(obj);
