1624119691d1ab92739a8da2a32771becf259d4d
QWei <30896013+00x16@users.noreply.github.com>
2020-10-27 13:12:42 +0800

    微信读书

---
+++ weread.js
@@ -0,0 +1,14 @@
+var body = $response.body;
+var url = $request.url;
+
+if (url.indexOf('/pay/memberCardSummary') != -1) {
+	var obj = JSON.parse(body);
+	obj..remainTime=9999999999999;
+	body = JSON.stringify(obj);
+} 
+
+body = JSON.stringify(obj);
+
+$done({
+	body
+});
